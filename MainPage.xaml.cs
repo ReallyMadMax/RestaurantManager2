4b15599714b415ae53a90bcf4f0602fc7bf10a0d
Max Dilger <ettrevivre@gmail.com>
2024-04-12 01:44:10 -0600

    pushing new version.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantManager
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
